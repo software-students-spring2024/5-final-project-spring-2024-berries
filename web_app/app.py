688a2be239534588fab2ed2a745da22456ba33ba
yurawu27 <cw3460@nyu.edu>
2024-04-25 03:02:01 -0400

    fixed unused api.py and added login & signup funcitons

--- web_app/app.py
+++ web_app/app.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from pymongo import MongoClient
 import requests
+import bcrypt
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+
+#----- NEEDS TO CHANGE SECRET KEY -----#
+app.secret_key = "323qssssa"
 
 # app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost:27017/coffee_shops')
 """
@@ -18,17 +22,25 @@ uri = "mongodb://mongodb:27017/"
 """
 
 try:
+    '''
     DB_USER = os.getenv("MONGODB_USER")
     DB_PASSWORD = os.getenv("MONGO_PWD")
     DB_HOST = os.getenv("DB_HOST")
     URI = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_HOST}.5kr79yv.mongodb.net/"
+    '''
+    URI = "mongodb+srv://bcdy:BPoOlpuLgv3WKJ62@coffeeshops.5kr79yv.mongodb.net/"
     client = MongoClient(URI)
-    db = client["cafes"]
-    gestureDB = db["cafes"]
+    db = client["coffeedb"]
+    #cafesCollection = db["coffee"]
+    user_collection = db["users"]
+    
+    # check if connected correctly
+    print(URI)
+    print("Connected!")
 except Exception as e:
-    print(e+ "THIS IS WHERE THE EORROR IS ")
+    print(e)
 
-
+'''
 try:
     # verify the connection works by pinging the database
     client.admin.command("ping")  # The ping command is cheap and does not require auth.
@@ -36,68 +48,94 @@ try:
 except Exception as e:
     # the ping command failed, so the connection is not available.
     print(" * ERRORRR", e)  # debug
+'''
 
 
-@app.route("/")
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    message = None
+    if request.method == 'POST':
+        new_name = request.form['name']
+        new_username = request.form['username']
+        new_password = request.form['password']
+
+        existing_user = user_collection.find_one({'username': new_username})
+        #existing_user = user_collection.find({})
+        #print(existing_user)
+        
+        if existing_user is None:
+            #hashpass = bcrypt.hashpw(new_password, bcrypt.gensalt())
+            hashpass = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+            user_collection.insert_one({'username': new_username, 'password': hashpass, 'name': new_name})
+            session['username'] = new_username
+            return redirect(url_for('home'))
+
+        
+        message = 'That username already exists!'
+        return render_template('sign-up.html', message=message)
+    
+    return render_template('sign-up.html', message=message) 
+
+
+
+# user login check
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    message = None
+    if request.method == 'POST':
+        login_user = user_collection.find_one({'username': request.form['username']})
+        
+        if login_user:
+            if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_user['password']):
+                session['username'] = request.form['username']
+                return redirect(url_for('home'))
+        else: 
+            message = 'User not found! Please register first.'
+            # return redirect(url_for('log-in'))
+            return render_template('log-in.html', message=message)
+        
+        message = 'Wrong username or password. Please try again.'    
+        return render_template('log-in.html', message=message)
+        # return redirect(url_for('login')) 
+    
+   #  return redirect(url_for('home'))
+    return render_template('log-in.html', message=message) 
+
+
+
+# user logout
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+
+@app.route("/home")
 def home():
-    """Render the home page."""
+    if 'username' not in session: # if user is not logged in
+        return redirect(url_for('login'))
+    
     return render_template("index.html")
 
 
-# get users location and forward to machine learning model
 @app.route("/find_coffee_shops", methods=["POST"])
 def find_coffee_shops():
-    """Get users location and forward to machine learning model."""
     data = request.get_json()
-
-    # get long and lat
     latitude = data.get("latitude")
     longitude = data.get("longitude")
-
-    if latitude is None or longitude is None:
-        return (
-            jsonify({"error": "Latitude and longitude are required parameters."}),
-            400,
-        )
-
-    # api key and parameters for google places
-    api_key = "AIzaSyC4jaf9Xb9_yFj-wl_hLJjL3CxXhGN1WfY"
-    radius = 400  # ADJUSTABLE
-    types = "cafe"  # search only cafes
-
-    # google api request
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&type={types}&key={api_key}"
-    response = requests.get(url, timeout=15)
-
-    if response.status_code == 200:
-        # get info from api
-        coffee_shops = []
-        for place in response.json().get("results", []):
-            # get a photo of coffeeshop
-            photo_reference = (
-                place["photos"][0]["photo_reference"]
-                if "photos" in place and len(place["photos"]) > 0
-                else None
-            )
-
-            coffee_shops.append(
-                {
-                    "name": place["name"],
-                    "vicinity": place.get(
-                        "vicinity"
-                    ),  # vicinity = address in google places
-                    "rating": place.get("rating"),
-                    "photo_reference": photo_reference,
-                }
-            )
-
-        return jsonify({"coffee_shops": coffee_shops})
-    return (
-        jsonify({"error": "Failed to fetch coffee shops from Google Places API."}),
-        500,
-    )
+    api_url = "http://localhost:5002/find_cafes"  # URL to google_api.py service
+    api_response = requests.post(api_url, json={"latitude": latitude, "longitude": longitude})
+    if api_response.status_code == 200:
+        return jsonify(api_response.json())
+    else:
+        return jsonify({"error": "Backend API failed"}), 500
 
 
+
+
+'''
+#---------------------THIS IS NEVER USED!!!----------------------------#
 # show all results
 @app.route("/results", methods=["POST"])
 def show_results():
@@ -106,10 +144,11 @@ def show_results():
     latitute = data["latitude"]
     longitude = data["longitude"]
 
-    coffee_shops = db.cafes.find({"latitute": latitute, "longitude": longitude})
+    coffee_shops = cafesCollection.find({"latitute": latitute, "longitude": longitude})
 
     shop_list = [shop for shop in coffee_shops]
     return render_template("results.html", coffee_shops=shop_list)
+'''
 
 
 # ---------------------------------------------------------------------------- #
